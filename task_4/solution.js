0476dcd6613105dd52181309c16e44f38a20b54a
SergeyMalkov81 <skydark@yandex.ru>
2022-02-04 02:20:38 +0400

    Домашнее задание 4 - требует дорабоотки

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,6 +7,20 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+
+    let data = JSON.parse('{}');
+    let products = data.products;
+
+    return products;
+}
+
+function renderProductsCards(json){
+    clearProducts();
+    let products = parseProducts(json);
+    length = products.length;
+for (let i = 0; i < length; i += 1) {
+    addProduct(products[i]);
+}
 }
 
 // Напишите функцию renderProductsCards(json)
